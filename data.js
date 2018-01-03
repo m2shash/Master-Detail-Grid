bedde32c5715a88fa2a8312c863b21e400199681
m2shash <m2shash@gmail.com>
2018-01-03 12:34:07 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,148 @@
+﻿var resellers = [{
+    "ID": 1,
+    "FirstName": "Company 1",
+    "LastName": "Heart",
+    "Prefix": "Reseller ABC",
+    "BundlePrice": "150",
+    "UnitPrice": "250",
+    "BillingAmount": "8475",
+    "Tasks": [{
+        "ID": 5,
+        "Device": "CPT TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/04/15",
+        "Status": "Good",
+        "Priority": "High",
+        "Completion": 75
+    }, {
+        "ID": 6,
+        "Device": "CPT 252 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/28",
+        "Status": "Offline",
+        "Priority": "High",
+        "Completion": 100
+    }, {
+        "ID": 7,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Good",
+        "Priority": "Normal",
+        "Completion": 100
+    }]
+}, {
+    "ID": 2,
+    "FirstName": "Company 2",
+    "LastName": "Peyton",
+    "Prefix": "Reseller1",
+    "BundlePrice": "150",
+    "UnitPrice": "250",
+    "BillingAmount": "4520",
+    "Tasks": [{
+        "ID": 3,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Completed",
+        "Priority": "High",
+        "Completion": 100
+    }, {
+        "ID": 4,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Good",
+        "Priority": "High",
+        "Completion": 50
+    }, {
+        "ID": 21,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Offline",
+        "Priority": "Low",
+        "Completion": 100
+    }, {
+        "ID": 50,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Offline",
+        "Priority": "Normal",
+        "Completion": 100
+    }]
+},
+
+{
+    "ID": 3,
+    "FirstName": "Company 3",
+    "Prefix": "Reseller IKAS ",
+    "BundlePrice": "178",
+    "UnitPrice": "655",
+    "BillingAmount": "98744",
+    "Tasks": [{
+        "ID": 16,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Warning",
+        "Priority": "High",
+        "Completion": 100
+    }, {
+        "ID": 74,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Offline",
+        "Priority": "High",
+        "Completion": 100
+    }, {
+        "ID": 78,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Good",
+        "Priority": "Normal",
+        "Completion": 100
+    }, {
+        "ID": 117,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Good",
+  
+    }]
+},
+{
+    "ID": 4,
+    "FirstName": "Company 4",
+    "Prefix": "Reseller JBD",
+    "BundlePrice": "224",
+    "UnitPrice": "998",
+    "BillingAmount": "1478855",
+    "Tasks": [{
+        "ID": 20,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Offline",
+     
+    }, {
+        "ID": 23,
+        "Device": "CPT 145 TC100",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Offline",
+        "Priority": "Normal",
+        "Completion": 90
+    }, {
+        "ID": 40,
+        "Device": "CPT 145 TC200",
+        "SMS Count DEC | JAN": "213|143",
+        "ActivationDate": "2013/02/24",
+        "Status": "Warning",
+
+    }]
+}]; 
+
---
+++ index.js
@@ -0,0 +1,92 @@
+﻿$(function () {
+    $("#gridContainer").dxDataGrid({
+        dataSource: resellers,
+        keyExpr: "ID",
+        rowAlternationEnabled: true,
+        allowFiltering: true,
+        showBorders: true,
+        showRowLines: true,
+        headerFilter: {
+            visible: true
+            
+        },
+        paging: {
+            pageSize: 10
+        },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [5, 10, 20],
+            showInfo: true
+        },
+        columns: [{
+            dataField: "Prefix",
+            caption: "Reseller",
+            cssClass: "rowColor",
+            width: 200
+        },
+            
+            {
+                dataField: "BundlePrice",
+                caption: "100 Bundle SMS Price",
+                cssClass: "rowColor",
+                width: 250
+            },
+            {
+                dataField: "UnitPrice",
+                caption: "Unit Price",
+                cssClass: "rowColor",
+                width: 150
+            },
+            {
+                dataField: "BillingAmount",
+                caption: "Billing Amount",
+                cssClass: "rowColor",
+                width: 150
+            }
+        ],
+        masterDetail: {
+            enabled: true,
+            template: function (container, options) {
+                var resellersData = options.data;
+
+                $("<div>")
+                    .addClass("master-detail-caption")
+                    .text(resellersData.FirstName)
+                    .appendTo(container);
+
+                $("<div>")
+                    .dxDataGrid({
+                        columnAutoWidth: true,
+                        showBorders: true,
+                        rowAlternationEnabled: true,
+                        columns: [
+                            {
+                                dataField: "Device",
+                                cssClass: 'cls'
+                            },
+                            {
+                                dataField: "SMS Count DEC | JAN",
+                                cssClass: 'cls'
+                             },
+                            {
+                                dataField: "ActivationDate",
+                                dataType: "date",
+                                cssClass: 'cls'
+                            },
+                            {
+                                dataField: "Status",
+                                cssClass: 'cls'
+                            }
+                        ],
+                        showRowLines: true,
+                        onRowPrepared: function (e) {
+                            
+                            e.rowElement.css({ "background-color": "GhostWhite" });
+                            
+                        },
+                        dataSource: resellersData.Tasks
+                    }).appendTo(container);
+            }
+        }
+    });
+});
